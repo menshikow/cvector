ea18ac0b6b0e18dc8b0d48e8a89258b400c77904
madonnaprayer <adriaanmenschikow@gmail.com>
2025-11-27 21:24:25 +0100

    added first functions

--- src/cvector.c
+++ src/cvector.c
@@ -1,6 +1,6 @@
 #include "../include/cvector.h"
-#include <stdio.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -10,32 +10,30 @@
 */
 
 int vec_init(Vec *v, size_t elem_size) {
-    v->size = 0;
-    v->capacity = 10;
-    v->elem_size = elem_size;
-    v->data = malloc(elem_size * v->capacity);
-    if (v->data == NULL) {
-        return VEC_ERR_OOB;
-    }
-    return VEC_OK;
+  v->size = 0;
+  v->capacity = 10;
+  v->elem_size = elem_size;
+  v->data = malloc(elem_size * v->capacity);
+  if (v->data == NULL) {
+    return VEC_ERR_OOB;
+  }
+  return VEC_OK;
 }
 
 int vec_push_back(Vec *v, const void *element) {
-    if (v->capacity == v->size) {
-        v->capacity *= 2;
+  if (v->capacity == v->size) {
+    v->capacity *= 2;
 
-        void *tmp = realloc(v->data, v->capacity * v->elem_size);
-        if (tmp == NULL) {
-            return VEC_ERR_OOB;
-        }
-        v->data = tmp;
+    void *tmp = realloc(v->data, v->capacity * v->elem_size);
+    if (tmp == NULL) {
+      return VEC_ERR_OOB;
     }
+    v->data = tmp;
+  }
 
-    // copy element bytes into the correct offset
-    memcpy((char *)v->data + v->size * v->elem_size,
-           element,
-           v->elem_size);
+  // copy element bytes into the correct offset
+  memcpy((char *)v->data + v->size * v->elem_size, element, v->elem_size);
 
-    v->size++;
-    return VEC_OK;
+  v->size++;
+  return VEC_OK;
 }
